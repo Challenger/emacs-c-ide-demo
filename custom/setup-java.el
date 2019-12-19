cbf311080e295162ab8e1ec8a43fdc5fee523b92
Шуляк Виктор Иванович <Shulyak.V.Iv@sberbank.ru>
2019-12-19 19:35:20 +0300

    LSP for Java added

--- custom/setup-java.el
+++ custom/setup-java.el
@@ -1,17 +1,34 @@
 
-(use-package meghanada
-  :bind
-  (:map meghanada-mode-map
-        (("C-M-o" . meghanada-optimize-import)
-         ("C-M-t" . meghanada-import-all)
-         )))
+;; (use-package meghanada
+;;   :bind
+;;   (:map meghanada-mode-map
+;;         (("C-M-o" . meghanada-optimize-import)
+;;          ("C-M-t" . meghanada-import-all)
+;;          )))
 
-(defun my-java-meghanda-mode-hook ()
-  (meghanada-mode)
-  (flycheck-mode))
+;; (defun my-java-meghanda-mode-hook ()
+;;   (meghanada-mode)
+;;   (flycheck-mode))
 
-(add-hook 'java-mode-hook 'my-java-meghanda-mode-hook)
+;; (add-hook 'java-mode-hook 'my-java-meghanda-mode-hook)
 
-(add-to-list 'auto-mode-alist '("\\.gradle\\'" . groovy-mode))
+;; (add-to-list 'auto-mode-alist '("\\.gradle\\'" . groovy-mode))
+
+(use-package projectile :ensure t)
+(use-package yasnippet :ensure t)
+(use-package lsp-mode :ensure t)
+(use-package hydra :ensure t)
+(use-package company-lsp :ensure t)
+(use-package lsp-ui :ensure t)
+(use-package lsp-java :ensure t :after lsp
+  :config (add-hook 'java-mode-hook 'lsp))
+
+(use-package dap-mode
+  :ensure t :after lsp-mode
+  :config
+  (dap-mode t)
+  (dap-ui-mode t))
+
+;; (use-package dap-java :after (lsp-java))
 
 (provide 'setup-java)
--- init.el
+++ init.el
@@ -72,6 +72,7 @@
      ("asm" . "*.[sS]")
      ("java" . "*.java")
      ("js" . "*.[jt]s"))))
+ '(helm-completion-style (quote emacs))
  '(helm-ff-lynx-style-map t)
  '(helm-grep-use-ioccur-style-keys t)
  '(helm-imenu-lynx-style-map t)
@@ -86,7 +87,7 @@
      ("melpa-stable" . "https://stable.melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (gnu-elpa-keyring-update flymake-json fish-completion fish-mode wgrep color-theme-sanityinc-tomorrow markdown-mode dired-rainbow rainbow-mode groovy-imports company-ansible company-emacs-eclim flymake-yaml yaml-imenu yaml-mode wgrep-ag wgrep-helm ag meghanada duplicate-thing feature-mode gradle-mode groovy-mode helm-ag web-mode tide company-web company-tern xref-js2 js2-mode json-mode magit zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu helm projectile smartparens comment-dwim-2 helm-projectile golden-ratio helm-swoop flymake-less less-css-mode)))
+    (helm-xref company-lsp helm-lsp lsp-java gnu-elpa-keyring-update flymake-json fish-completion fish-mode wgrep color-theme-sanityinc-tomorrow markdown-mode dired-rainbow rainbow-mode groovy-imports company-ansible company-emacs-eclim flymake-yaml yaml-imenu yaml-mode wgrep-ag wgrep-helm ag meghanada duplicate-thing feature-mode gradle-mode groovy-mode helm-ag web-mode tide company-web company-tern xref-js2 js2-mode json-mode magit zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu helm projectile smartparens comment-dwim-2 helm-projectile golden-ratio helm-swoop flymake-less less-css-mode)))
  '(prog-mode-hook
    (quote
     (turn-on-smartparens-strict-mode ws-butler-mode clean-aindent-mode yas-minor-mode
@@ -124,4 +125,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 85 :family "DejaVu Sans Mono")))))
+ '(default ((t (:height 85 :family "DejaVu Sans Mono"))))
+ '(lsp-ui-sideline-code-action ((t (:foreground "dark orange")))))
